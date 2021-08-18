10ae96164f8c208f3e7ceb2c8bc7bf98113dc5c7
AppleMan05 <aupathak1234@gmail.com>
2021-08-18 23:11:21 +0530

    Added a semi working remind command

--- rewrite.py
+++ rewrite.py
@@ -19,6 +19,7 @@ async def RemindFunction(user_id, time, reason):
     }
     file = open("database.json", "w")
     json.dump(my_dictionary, file)
+    file.close()
 @bot.event
 async def on_ready():
     print('Bot is up')
@@ -26,9 +27,9 @@ async def on_ready():
 @bot.command(name='neofetch')
 async def neofetch(ctx):
     await ctx.send("```please wait for 5 seconds, processing your input```")
-    x = subprocess.Popen('neofetch --stdout > /path/to/file', stdout=subprocess.PIPE, shell=True)
+    x = subprocess.Popen('neofetch --stdout > /path', stdout=subprocess.PIPE, shell=True)
     time.sleep(5)
-    file = open ("/path/to/file", "r")
+    file = open ("/path", "r")
     output = ""
     for line in file.readlines():
         output += line
@@ -55,12 +56,34 @@ async def google(ctx, *, query):
         embed.add_field(name="Result", value=j)
     await ctx.send(content=None, embed=embed)
 
-#@bot.command(name='remindme')
-#async def remindme(ctx, time, reason):
- #   userID = ctx.message.author.id
-  #  time = int(time)
-  #  new_time = datetime.now() + timedelta(hours=time)
-  #  bot.loop.create_task(RemindFunction(userID, new_time, reason))
+@bot.command(name='remindme')
+async def remindme(ctx, time_here, *, reason):
+    userID = ctx.message.author.id
+    time_here = int(time_here)
+    new_time = datetime.now() + timedelta(hours=time_here)
+    bot.loop.create_task(RemindFunction(userID, new_time, reason))
+    time.sleep(5)
+    json_file = open("database.json", "r")
+    final_file = json.load(json_file)
+    print(final_file[f'{ctx.message.author.id}'])
+    variable_defined = final_file[f'{ctx.message.author.id}']
 
+    
+        
+    if variable_defined is not None:
+        try:
+            await ctx.send(variable_defined['time'])
+            time_to_remind = variable_defined['time']
+        except KeyError:
+            await ctx.send("Invalid")
+            
+    now = datetime.now()
+    while time_to_remind is not None:
+        if now.strftime("%H %d %m %Y") >= time_to_remind:
+            await ctx.send(f'hello {ctx.author.mention}, you asked me to remind you for {reason}')
+            break 
+        else:
+            continue
+        time.sleep(5)
 
 bot.run("")
